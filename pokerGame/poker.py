b90cac8db0165cb00c7630f8734ef1e07625af5b
Christopher Wong <cwong239@calpoly.edu>
2025-02-05 11:59:45 -0800

    poker thing

---
+++ pokerGame/poker.py
@@ -0,0 +1,16 @@
+""""
+Game class
+    Handle checking for stuff
+    rounds
+Player Class
+    Functions
+        Fold
+        raise
+        Check
+    Variaels
+        Hands
+        Money
+        Bet
+    Agent Inherits that
+
+"""
